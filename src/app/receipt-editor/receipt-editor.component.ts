06920fec3df70842a9f8224c245f45b39948edc6
ckm891129 <ckm_kent@hotmail.com>
2016-09-28 10:21:36 +0800

    bind data
    Bind data to the receipt-editor form with ngModel

--- src/app/receipt-editor/receipt-editor.component.ts
+++ src/app/receipt-editor/receipt-editor.component.ts
@@ -1,5 +1,14 @@
 import { Component, OnInit } from '@angular/core';
 
+// Step 1 for get the data from router
+// import {} from first to show autocomplete
+import { ActivatedRoute, Params } from '@angular/router'
+
+
+// import the orderservices. then order info
+import { OrderService } from '../order.service'
+import { Order } from '../order'
+
 @Component({
   selector: 'app-receipt-editor',
   templateUrl: './receipt-editor.component.html',
@@ -7,9 +16,27 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ReceiptEditorComponent implements OnInit {
 
-  constructor() { }
+  // will run first when create instance ---> 1
+  // step 2 inject ActivatedRoute in Component constructor as route
+  constructor(private route:ActivatedRoute, private myOrderService:OrderService) { }
+  
+  // create formDate as string
+  private formDate:string;
+  private order:Order;
 
+  // will run when create component ---> 2
   ngOnInit() {
+    this.route.params.forEach((params: Params) => {
+      // information we need is the id
+      console.log(params['id']);
+      this.order = this.myOrderService.getOrder(params['id']);
+      // log the order info from the Order
+      console.log(this.order)
+
+      this.formDate = this.order.create_time.toISOString().substring(0,10);
+      console.log(this.formDate)
+    })
+
   }
 
 }
